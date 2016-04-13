4de793dc037f44d01ee8a7f775162dc02a7975ed
Adam Schaal <amschaal@ucdavis.edu>
2016-04-13 16:47:14 -0700

    work on subscribed notifications, emails, etc

--- context_processors.py
+++ context_processors.py
@@ -3,7 +3,5 @@ from models import Notification
 def notifications(request):
     if not request.user.is_authenticated():
         return {}
-    user_notifications = Notification.objects.filter(user=request.user,seen__isnull=True,aggregate__isnull=True).order_by('-id')
-    
-#     user_notifications = get_notifications(user=request.user)
-    return {'user_notifications':user_notifications}
+    notifications = Notification.objects.filter(user=request.user,seen__isnull=True,aggregate__isnull=True).order_by('-id')
+    return {'notifications':notifications}
